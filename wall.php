dee0c2ecacc77fbd1c73047a26ce4e38875c4fec
Alexander van Keulen <Alexander@Alexanders-MacBook-Pro.local>
2016-03-31 14:02:26 +0200

    wall

---
+++ wall.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Instagram</title>
+    <link rel="stylesheet" href="css/instagram.css">
+</head>
+
+<body>
+
+    <nav id="navigatie">
+        <p>EXPERIENCE</p>
+        <img id="logoinsta" src="images/Instragram%20logo%202013.png">
+    </nav>
+
+    <section class="login-form-wrap" style="height: 156px">
+        <p class="message">Welcome back!</p>
+        <a style="color: black" href="logout.php">Logout</a>
+    </section>
+
+
+    <section class="">
+        <ul>
+            <li class="wallpost">
+                <div id="img-holder">
+
+                </div>
+
+                <div id="comment-section">
+                    <p>This is a comment!! #YOLO</p>
+                    <form action="">
+                        <a href="#" id="like-btn">Like</a>
+                        <input type="text" placeholder="Comment" id="activitymessage" name="activitymessage" />
+                        <input id="btnSubmit" type="submit" value="Place" />
+                    </form>
+                </div>
+            </li>
+        </ul>
+    </section>
+
+
+</body>
+
+</html>
