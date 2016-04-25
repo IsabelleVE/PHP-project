73074bb6b72860081e9c4ceb997af2ea32e4fd0e
Isabelle Van Eyken <isabelle@MacBook-Pro-van-Isabelle.local>
2016-04-25 11:37:08 +0200

    Dit is de nieuwe login

--- login.php
+++ login.php
@@ -1,61 +1,23 @@
 <?php
+include_once ("classes/User.class.php");
 
-// function canLogin
-// cookies staan op de gebruiker zijn/haar pc => waardoor manipuleerbaar 
 
-function canLogin( $p_username, $p_password ){
-    if( $p_username == "samuel@sieben.com" && $p_password == "secret" ){
-        
-        return true;
-    }
-    else{
-        
-        return false;
-    }
-    
-}
 
-// if structure 
-
-if( !empty( $_POST ) ){
-    
-$username = $_POST['email'];
-$password = $_POST['password'];
-
-    if( canLogin( $username, $password ) ){
-        
-        session_start(); 
-        $_SESSION['loggedin'] = "yes";
-        
-        
-        /* 
-        
-        // Vorige Cookieoplossing
-        
-        $salt = "fnRfrhJdbdfb!!!!dnkfbsOnkmqkn!!Bd!d";
-        $secret = md5( $username.$salt );
-        $cookieContent = $username.",".$secret; 
-
-        // remember user
-        
-        setcookie( "loggedin", $cookieContent , time()+60*60*24*30); // cookie van 1 maand 
-        */
-        
-        // redirect to index.php
-        
-        header('location: index.php');
-        
-    }  
-    else{
-        // feedback
-        $error = "Wow stop! You are using wrong input"; 
+if(!empty($_POST)) {
+    try {
+        $u = new User();
+        $u->UserName = $_POST['username'];
+        $u->Password = $_POST['password'];
+        if ($u->canLogin()) {
+            session_start();
+            $_SESSION['loggedin'] = $_POST['username'];
+            header('location: index.php');
+        }
+    } catch (Exception $e) {
+        $feedback = $e->getMessage();
     }
 }
-
-
-?>
-
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -64,34 +26,34 @@ $password = $_POST['password'];
     <link rel="stylesheet" href="css/instagram.css">
 </head>
 <body>
-        
-       <nav id="navigatie">
-           <p>WELCOME TO</p>
-           <img id="logoinsta" src="images/Instragram%20logo%202013.png">
-       </nav>
-       
-        <section class="login-form-wrap">
-          <h1>Welcome to Instagram! Log In</h1>
-          
-          <?php
-            
-          if( isset($error) ){
-              
-              echo "<p class='error' > $error </p>";
-          }
-            
-            ?>
-          
-          <form class="login-form" method="POST" action="">
-            <label>
-              <input type="email" name="email" placeholder="Email">
-            </label>
-            <label>
-              <input type="password" name="password" placeholder="Password">
-            </label>
-            <input type="submit" value="Login">
-          </form>
-          <h5><a href="#">Forgot password</a></h5>
-        </section>
+
+<nav id="navigatie">
+    <p>WELCOME TO</p>
+    <img id="logoinsta" src="images/Instragram%20logo%202013.png">
+</nav>
+
+<section class="login-form-wrap">
+    <h1>Welcome to Instagram! Log In</h1>
+
+    <?php
+
+    if( isset($feedback) ){
+
+        echo "<p class='error' > $feedback </p>";
+    }
+
+    ?>
+
+    <form class="login-form" method="POST" action="">
+        <label>
+            <input type="text" name="username" placeholder="Username">
+        </label>
+        <label>
+            <input type="password" name="password" placeholder="Password">
+        </label>
+        <input type="submit" value="Login">
+    </form>
+    <h5><a href="#">Forgot password</a></h5>
+</section>
 </body>
 </html>
