532b7c3f693710eb4d4e1a70df66f35b4a2ebbe6
SamuelSieben <samuelsieben@outlook.com>
2016-05-11 16:41:19 +0200

    LIVESEARCH
    Live Search aangemaakt op profilepage.php

--- profilepage.php
+++ profilepage.php
@@ -51,6 +51,36 @@ echo $msg . "<br />";
     <!-- LAYOUT BY ED BOND: http://codepen.io/edbond88/pen/yGjAu -->
     <link rel="stylesheet" href="css/instagram.css">
     
+    
+    
+    
+    <script>
+function showResult(str) {
+  if (str.length==0) { 
+    document.getElementById("livesearch").innerHTML="";
+    document.getElementById("livesearch").style.border="0px";
+    return;
+  }
+  if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+  } else {  // code for IE6, IE5
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange=function() {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+      document.getElementById("livesearch").innerHTML=xmlhttp.responseText;
+      document.getElementById("livesearch").style.border="1px solid #A5ACB2";
+    }
+  }
+  xmlhttp.open("GET","livesearch.php?q="+str,true);
+  xmlhttp.send();
+}
+</script>
+    
+    
+    
+    
     <style type="text/css">
         
        
@@ -161,10 +191,15 @@ echo $msg . "<br />";
     </div>
 
 </nav>
-<form  method="post" action="search.php?go"  id="searchform">
-    	      <input  type="text" name="name">
-             <input  type="submit" name="submit" value="Search">
-    	    </form>
+
+<!-- SEARCH -->
+
+<form>
+<input type="text" size="30" onkeyup="showResult(this.value)">
+<div id="livesearch"></div>
+</form>
+
+<!-- END SEARCH -->
 
 <section class="login-form-wrap2">
 
