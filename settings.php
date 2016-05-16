4f9805819b83faafd3ff2f04180cdaa79a35d2f7
SamuelSieben <samuelsieben@outlook.com>
2016-05-16 20:56:06 +0200

    committing

--- settings.php
+++ settings.php
@@ -52,119 +52,8 @@ if( $statement->rowCount() > 0){
     <link rel="stylesheet" href="css/instagram.css">
     
     <style type="text/css">
-        
-        	.login-form-wrap2 {
-        display: block;
-    background: grey;
-    background: -moz-radial-gradient(center, ellipse cover, white 0%, white 100%);
-    background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(0%, white), color-stop(100%, white));
-    background: -webkit-radial-gradient(center, ellipse cover, white 0%, white 100%);
-    background: -o-radial-gradient(center, ellipse cover, white 0%, white 100%);
-    background: -ms-radial-gradient(center, ellipse cover, white 0%, white 100%);
-    background: radial-gradient(ellipse at center, white 0%, white 100%);
-    filter: progid: DXImageTransform.Microsoft.gradient( startColorstr='lightblue', endColorstr='lightblue', GradientType=1);
-    border: 1px solid white;
-    box-shadow: 0 1px white inset, 0 0 10px 5px rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    /* relative */
-    width: 560px;
-    height: 780px;
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: 30px;
-    padding: 50px 30px 0 30px;
-    text-align: center;
-}
-        
-        .profileicon{
-            display: inline-block;
-            width: 40px;
-            height: 40px;
-            margin-left: auto;
-            margin-right: 20px;
-        }
-        
-        .settingsicon{
-            display: inline-block;
-            width: 40px;
-            height: 40px;
-            margin-left: auto;
-            margin-right: 40px;
-        }
-        
-        .uppericons{
-            float: right;
-            margin-top: -60px;
-        }
-        
-        .logoutnav{
-            float: left;
-            display: inline-block;
-            margin-right: auto;
-            margin-left: 50px;
-            margin-top: -50px;
-        }
-        
-        
-        
-        
-.textbox{
-  height:50px;
-  width:100%;
-  border-radius:3px;
-  border:rgba(0,0,0,.3) 2px solid;
-  box-sizing:border-box;
-  padding:10px;
-  margin-bottom:30px;
-}
 
-.textbox:focus{
-  outline:none;
-   border:rgba(24,149,215,1) 2px solid;
-   color:rgba(24,149,215,1);
-}
-
-.button{
-  height:50px;
-  width:100%;
-  border-radius:3px;
-  border:rgba(0,0,0,.3) 0px solid;
-  box-sizing:border-box;
-  padding:10px;
-  margin-bottom:30px;
-  background: #3f729b;
-  color:#FFF;
-  font-weight:bold;
-  font-size: 12pt;
-  transition:background .4s;
-  cursor:pointer;
-}
-
-.button:hover{
-  background:#80b438;
-  
-}
-        
-        
-        footer{
-    background-color: #3f729b;
-    display: block;
-    color: white;
-    border-top-left-radius: 2em;
-    margin-top: 3em;
-    border-top-right-radius: 2em;
-    padding-bottom: 1em;
-    text-align: center;
-    
-}
-
-footer h3
-{
-    margin-top: 1em;
-}
-
-        
-	</style> 
+    </style>
 	
 </head>
 
@@ -186,7 +75,7 @@ footer h3
     
 
     
-    <section class="login-form-wrap2">
+    <section class="login-form-wrap3">
 
         <form class="password-form" method="POST" action="">
             <label>firstname :
@@ -205,7 +94,7 @@ footer h3
             <label>
                 <input class="textbox" type="password" name="newPassword" placeholder="New Password">
             </label>
-            <input class="button" type="submit" value="Reset">
+            <input class="buttonReset" type="submit" value="Reset">
         </form>
     </section>
 
