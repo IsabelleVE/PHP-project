d9e5db34f7a93d164ae90a3ce3c8409ca1ef81d3
IsabelleVE <isabelle.vaneyken@gmail.com>
2016-05-16 21:15:42 +0200

    profielpage terug inorde

--- profilepage.php
+++ profilepage.php
@@ -22,11 +22,12 @@ $u = new User();
     <title>Instagram Profile Page</title>
     <!-- LAYOUT BY ED BOND: http://codepen.io/edbond88/pen/yGjAu -->
     <link rel="stylesheet" href="css/instagram.css">
-    
+
     
     
     
     <script>
+
 function showResult(str) {
   if (str.length==0) { 
     document.getElementById("livesearch").innerHTML="";
@@ -49,8 +50,129 @@ function showResult(str) {
   xmlhttp.send();
 }
 </script>
+    <style type="text/css">
 
-    
+
+        .login-form-wrap2 {
+            background: grey;
+            background: -moz-radial-gradient(center, ellipse cover, white 0%, white 100%);
+            background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(0%, white), color-stop(100%, white));
+            background: -webkit-radial-gradient(center, ellipse cover, white 0%, white 100%);
+            background: -o-radial-gradient(center, ellipse cover, white 0%, white 100%);
+            background: -ms-radial-gradient(center, ellipse cover, white 0%, white 100%);
+            background: radial-gradient(ellipse at center, white 0%, white 100%);
+            filter: progid: DXImageTransform.Microsoft.gradient( startColorstr='lightblue', endColorstr='lightblue', GradientType=1);
+            border: 1px solid white;
+            box-shadow: 0 1px white inset, 0 0 10px 5px rgba(0, 0, 0, 0.1);
+            border-radius: 5px;
+            /* relative */
+            max-width: 860px;
+            margin-left: auto;
+            margin-right: auto;
+            margin-top: 30px;
+            padding: 50px 30px 0 30px;
+            text-align: center;
+        }
+
+        .profileicon{
+            display: inline-block;
+            width: 40px;
+            height: 40px;
+            margin-left: auto;
+            margin-right: 20px;
+        }
+
+        .settingsicon{
+            display: inline-block;
+            width: 40px;
+            height: 40px;
+            margin-left: auto;
+            margin-right: 40px;
+        }
+
+        .uppericons{
+            float: right;
+            margin-top: -60px;
+        }
+
+        .logoutnav{
+            float: left;
+            display: inline-block;
+            margin-right: auto;
+            margin-left: 50px;
+            margin-top: -50px;
+        }
+
+
+        /* Afbeelding Vrouw */
+
+        .profilepic{
+            display: block;
+            height: 100px;
+            width: 100px;
+            float:left;
+            margin-top: 30px;
+            margin-left: 20px;
+        }
+
+        .accountname{
+            text-align: left;
+            color: #3f729b;
+
+        }
+
+        .description{
+            text-align: left;
+        }
+
+        .accountanddiscription{
+            display: block;
+            float: left;
+            width: 70%;
+            margin-left: 80px;
+        }
+
+        .editprofile{
+            display: block;
+            height: 50px;
+            width: 100px;
+        }
+
+        footer{
+            background-color: #3f729b;
+            display: block;
+            color: white;
+            border-top-left-radius: 2em;
+            margin-top: 3em;
+            border-top-right-radius: 2em;
+            padding-bottom: 1em;
+            text-align: center;
+
+        }
+
+        .search{
+            border: 2px solid #3f729b;
+            border-radius: 1em;
+        }
+        .userPhotos{
+            clear: both;
+            display: flex;
+            justify-content: space-between;
+            flex-wrap: wrap;
+        }
+        .userPhotos div{
+            width: 32%;
+            margin-bottom: 15px;
+        }
+        img {
+            max-width: 100%;
+            max-height: 100%;
+            width: auto;
+            height: auto;
+        }
+    </style>
+
+
 </head>
 <body>
 <nav id="navigatie">
